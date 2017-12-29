2ea3f208be6c3d34a6c405739025791f22880e77
Mooga <anandmooga2@gmail.com>
2017-12-29 17:17:03 +0000

    Change defination of co-occurance to count of unique filenames

--- GraphCreate.sql
+++ GraphCreate.sql
@@ -6,7 +6,7 @@ CREATE TABLE Extracted( Filename INTEGER, Lemma TEXT, CNG INTEGER, Word TEXT, In
 INSERT INTO Extracted(Filename, Lemma, CNG, Word, Index1, Index2)
 SELECT Filename, Lemma, CNG, Lemma || '_' || substr(CNG, -4, 4), Index1, Index2
 FROM MyTable ;
-DROP MyTable;
+
 
 --To define realtions between Lemma, CNG and Word we create a Graph Database with weighted edges, nodes being CNG, Lemma and Word. We have 9 diffrent edges.
 --This is possible is sql with innerjoins.
@@ -27,35 +27,35 @@ CREATE TABLE Graph( Entity_1 TEXT, Type_1 TEXT, Entity_2 TEXT, Type_2 TEXT, Weig
 
 --Lemma Lemma Edge
 INSERT INTO Graph( Entity_1, Type_1, Entity_2, Type_2, Weight)
-SELECT e.Lemma, 'Lemma', n.Lemma, 'Lemma', COUNT(e.Filename)
+SELECT e.Lemma, 'Lemma', n.Lemma, 'Lemma', COUNT(DISTINCT e.Filename)
 FROM Extracted e, Node n
 WHERE e.Filename = n.Filename AND e.rowid <> n.rowid
 GROUP BY e.Lemma, n.Lemma;
 
 --Lemma CNG Edge
 INSERT INTO Graph( Entity_1, Type_1, Entity_2, Type_2, Weight)
-SELECT e.Lemma, 'Lemma', substr(n.CNG, -4, 4), 'CNG', COUNT(e.Filename)
+SELECT e.Lemma, 'Lemma', substr(n.CNG, -4, 4), 'CNG', COUNT(DISTINCT e.Filename)
 FROM Extracted e, Node n
 WHERE e.Filename = n.Filename AND e.rowid <> n.rowid
 GROUP BY e.Lemma, n.CNG;
 
 --Lemma Word Edge
 INSERT INTO Graph( Entity_1, Type_1, Entity_2, Type_2, Weight)
-SELECT e.Lemma, 'Lemma', n.Word, 'Word', COUNT(e.Filename)
+SELECT e.Lemma, 'Lemma', n.Word, 'Word', COUNT(DISTINCT e.Filename)
 FROM Extracted e, Node n
 WHERE e.Filename = n.Filename AND e.rowid <> n.rowid
 GROUP BY e.Lemma, n.Word;
 
 --CNG CNG Edge
 INSERT INTO Graph( Entity_1, Type_1, Entity_2, Type_2, Weight)
-SELECT substr(e.CNG, -4, 4), 'CNG', substr(n.CNG, -4, 4), 'CNG', COUNT(e.Filename)
+SELECT substr(e.CNG, -4, 4), 'CNG', substr(n.CNG, -4, 4), 'CNG', COUNT(DISTINCT e.Filename)
 FROM Extracted e, Node n
 WHERE e.Filename = n.Filename AND e.rowid <> n.rowid
 GROUP BY e.CNG, n.CNG;
 
 --CNG Word Edge
 INSERT INTO Graph( Entity_1, Type_1, Entity_2, Type_2, Weight)
-SELECT substr(e.CNG, -4, 4), 'CNG', n.Word, 'Word', COUNT(e.Filename)
+SELECT substr(e.CNG, -4, 4), 'CNG', n.Word, 'Word', COUNT(DISTINCT e.Filename)
 FROM Extracted e, Node n
 WHERE e.Filename = n.Filename AND e.rowid <> n.rowid
 GROUP BY e.CNG, n.Word;
@@ -63,7 +63,7 @@ GROUP BY e.CNG, n.Word;
 
 --Word Word Edge
 INSERT INTO Graph( Entity_1, Type_1, Entity_2, Type_2, Weight)
-SELECT e.Word, 'Word', n.Word, 'Word', COUNT(e.Filename)
+SELECT e.Word, 'Word', n.Word, 'Word', COUNT(DISTINCT e.Filename)
 FROM Extracted e, Node n
 WHERE e.Filename = n.Filename AND e.rowid <> n.rowid
 GROUP BY e.Word, n.Word;
@@ -71,14 +71,14 @@ GROUP BY e.Word, n.Word;
 
 --CNG Lemma Edge
 INSERT INTO Graph( Entity_1, Type_1, Entity_2, Type_2, Weight)
-SELECT substr(e.CNG, -4, 4), 'CNG', n.Lemma, 'Lemma', COUNT(e.Filename)
+SELECT substr(e.CNG, -4, 4), 'CNG', n.Lemma, 'Lemma', COUNT(DISTINCT e.Filename)
 FROM Extracted e, Node n
 WHERE e.Filename = n.Filename AND e.rowid <> n.rowid
 GROUP BY e.CNG, n.Lemma;
 
 --Word CNG Edge
 INSERT INTO Graph( Entity_1, Type_1, Entity_2, Type_2, Weight)
-SELECT e.Word, 'Word', substr(n.CNG, -4, 4), 'CNG', COUNT(e.Filename)
+SELECT e.Word, 'Word', substr(n.CNG, -4, 4), 'CNG', COUNT(DISTINCT e.Filename)
 FROM Extracted e, Node n
 WHERE e.Filename = n.Filename AND e.rowid <> n.rowid
 GROUP BY e.Word, n.CNG;
